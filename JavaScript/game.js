91d0af278f07d507c96b1c41eb6a418107c49577
safiullah7 <iamsafiullah@gmail.com>
2020-07-04 08:58:35 +0500

    removing useless code

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -8,26 +8,7 @@ export default class Game {
     startGame(rows, columns) {
         let player1 = new Player('P1', 'viking1');
         let player2 = new Player('P2', 'viking2');
-        // let game = this;
         let grid = new Grid(rows, columns, player1, player2, this.status);
         grid.Generate();
-    }
-    UpdateProgressBar(id, power) {
-        if (power < 0)
-            power = 0;
-        $('#' + id).val(power);
-        if (power == 0 && id.includes('p1')) {
-            if (confirm("Player 2 won! To restart game, press OK")) {
-                location.reload();
-              } else {
-                txt = "You pressed Cancel!";
-            }
-        } else if (power == 0 && id.includes('p2')) {
-            if (confirm("Player 1 won! To restart game, press OK")) {
-                location.reload();
-              } else {
-                txt = "You pressed Cancel!";
-            }
-        }
     }
 }
