e0eb31bd867cec3dee772e992e72115eff5ed91d
safiullah7 <iamsafiullah@gmail.com>
2020-07-04 09:08:51 +0500

    removing useless commented code

--- JavaScript/cell.js
+++ JavaScript/cell.js
@@ -8,18 +8,9 @@ export default class Cell {
         this.x = Math.floor(index / 10);
         this.y = index % 10;
         this.weapon = null;
-        // weapon property?
         this.player = null;
-        // player property?
         this.blocker = false;
-
         this.html = null;
-
-        // player
-        // blockage
-        // weapon
-
-        // i cud save html element as property 
     }
     Generate(id) {
         let cell = document.createElement('div');
@@ -33,18 +24,15 @@ export default class Cell {
     }
     setBackgroundBlocker() {
         this.blocker = true;
-        // this.html.classList.add('blocker');
         $(this.html).addClass("blocker");
     }
     setWeapon(className) {
         this.weapon = new Weapon()
         this.weapon.setWeaponTypeFromClass(className);
         console.log(className);
-        // this.html.classList.add(className);
         $(this.html).addClass(className);
     }
     setPlayer(cssClass) {
-        // this.html.classList.add(cssClass);
         $(this.html).addClass(cssClass);
     }
 }
--- JavaScript/player.js
+++ JavaScript/player.js
@@ -4,7 +4,6 @@ export default class Player {
     constructor(name, avatar) {
         this.name = name;
         this.avatar = avatar;
-        // cell prop. which cell player is on
         this.cell = null;
         this.weapon = null;
         this.health = 100;
